a4af4899281cec596b0a578c14136d2d9d400454
Callum Muir <steppinyello@gmail.com>
2015-11-18 16:38:31 +0000

    file now creates correct html, incorrectly indented for now.

--- text_to_html/lib/text_to_html.rb
+++ text_to_html/lib/text_to_html.rb
@@ -3,10 +3,12 @@ def read_file(stringFileLoc)
 end
 
 def output_string_to_file(outputString)
+	outputLoc = "../bin/output/outExample.txt"
+
 	# TODO change from default?
 	# TODO add error checking, false if fail, true if not
 	# TODO add in checking for if the file is already in use. Then create new file W/date, or title from user?
-	outputLoc = "outExample.txt"
+	
 	outputFile = File.new(outputLoc, "w")
 	outputFile.puts(outputString)
 end
@@ -17,12 +19,49 @@ end
 
 def split_string_by_para(stringIn)
 	paraArray = stringIn.split("\n")
-	puts paraArray[0]
 	return paraArray
 end
 
+# first para = title <h2>
+# all subsequent paragraps are paragraphs
+def add_html_to_string(arrayOfPara)
+	# Add start of the HTML
+	outString = 
+		"<div class=\"post col-xs-12\">
+		  <div class=\"col-md-3 col-xs-1\">
+		  </div>
+
+		  <div class=\"col-md-6 col-xs-10\">
+		  	<h2>" + arrayOfPara[0] + "</h2>"
+	# Add each paragraph
+	(1..(arrayOfPara.length - 1)).each do |i|
+		if(arrayOfPara[i].length < 1)
+			
+		else 
+			outString = outString + 
+			  "<p>" + arrayOfPara[i] + "
+			  </p> 
+			  <br />
+
+			  "
+		end
+	end
 
 
+
+	# Add end of the HTML 
+	outString = outString + 
+    "</div> 
+     <div class=\"col-md-3 col-xs-1\">
+     </div>
+     <div class=\"seperator col-xs-12\">
+     <p>------------------------------x------------------------------</p>
+     </div>
+    </div>
+		"
+
+	return outString
+end
 
 
 
@@ -32,17 +71,9 @@ end
 fileLoc = "example.txt"
 fileIn = read_file(fileLoc)
 stringContent = file_to_string(fileIn)
-split_string_by_para(stringContent)
-# TODO here for more functions!
-
-# TODO parse the file
-
-
-# TODO Add in html tags
-
-
-
-output_string_to_file(stringContent)
+paragraphs = split_string_by_para(stringContent)
+outString = add_html_to_string(paragraphs)
+output_string_to_file(outString)
 
 
 
