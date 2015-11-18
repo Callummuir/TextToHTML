37e79b3518dc490867e5fba0ea20f1621abdb709
Callum Muir <steppinyello@gmail.com>
2015-11-18 15:17:06 +0000

    Project method created, just needs filling in

--- text_to_html/lib/text_to_html.rb
+++ text_to_html/lib/text_to_html.rb
@@ -1,16 +1,38 @@
 
 
-
+# Reads in a file with given location
 def read_file(fileToRead)
-	txt = open(fileToRead)
-  puts "Here's your file #{fileToRead}:"
-  print txt.read
+	return open(fileToRead, "rb")
 end
 
+# ouputs a string into the correct output file
+def output_file(fileToOutput)
+	outputLoc = "outExample.txt"
+	outputFile = File.new("outputLoc", "w")
+	outputFile.puts(fileToOutput)
+end
 
+# Convert file to a string
+def file_to_string(file)
+	fileContent = file.read	
+end
+
+# TODO add user input for text file
 filename = "example.txt"
 
+# read in the file
 read_file(filename)
+
+# parse the file
+
+
+# Add in html tags
+
+
+# convert to string
+
+
+# output
 
 
 
